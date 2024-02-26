5e5309b13e52d2162347a0c1527d4069e9e1235d
Dragon Liu <dragonliu2018@gmail.com>
2024-02-26 15:57:15 +0800

    init repo

---
+++ config.sh
@@ -0,0 +1,8 @@
+# pip 临时换源
+alias cpip="pip install -i https://pypi.tuna.tsinghua.edu.cn/simple"
+# 递归地抓取子模块的更改
+alias srgit="git submodule update --init --recursive"
+# git 设置 name
+alias set-name="git config --global user.name 'Dragon Liu'"
+# git 设置 email
+alias set-email="git config --global user.email 'dragonliu2018@gmail.com'"
